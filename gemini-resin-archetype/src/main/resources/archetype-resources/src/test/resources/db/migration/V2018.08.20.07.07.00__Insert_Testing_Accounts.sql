9aaad3ecbb5fa2ce8fded31613f86e7254c414e5
msmith <msmith@techempower.com>
2018-12-05 11:02:53 -0800

    Fixes JWT not working with test users out of box

--- gemini-resin-archetype/src/main/resources/archetype-resources/src/test/resources/db/migration/V2018.08.20.07.07.00__Insert_Testing_Accounts.sql
+++ gemini-resin-archetype/src/main/resources/archetype-resources/src/test/resources/db/migration/V2018.08.20.07.07.00__Insert_Testing_Accounts.sql
@@ -1,9 +1,9 @@
 /* Inserts some testing user accounts.  Delete these in a production environment. */
 
 /* Username / Password: user / test */
-INSERT INTO "user" (userusername, userfirstname, userlastname, useremail, userpassword, enabled) VALUES ('user', 'first', 'last', 'none-user@techempower.com', '$2a$12$OepZj8bxnM9vrVHr1x7/k.6gE8QJZ8OKLTs7PPFhUopilS2RmQjq.', true);
+INSERT INTO "user" (userusername, userfirstname, userlastname, useremail, userpassword, enabled, userlastpasswordchange) VALUES ('user', 'first', 'last', 'none-user@techempower.com', '$2a$12$OepZj8bxnM9vrVHr1x7/k.6gE8QJZ8OKLTs7PPFhUopilS2RmQjq.', true, now());
 /* Username / Password: admin / test */
-INSERT INTO "user" (userusername, userfirstname, userlastname, useremail, userpassword, enabled) VALUES ('admin', 'first', 'last', 'none-admin@techempower.com', '$2a$12$OepZj8bxnM9vrVHr1x7/k.6gE8QJZ8OKLTs7PPFhUopilS2RmQjq.', true);
+INSERT INTO "user" (userusername, userfirstname, userlastname, useremail, userpassword, enabled, userlastpasswordchange) VALUES ('admin', 'first', 'last', 'none-admin@techempower.com', '$2a$12$OepZj8bxnM9vrVHr1x7/k.6gE8QJZ8OKLTs7PPFhUopilS2RmQjq.', true, now());
 
 INSERT INTO usertogroup (userid, groupid) VALUES (1, 1);
 INSERT INTO usertogroup (userid, groupid) VALUES (2, 1);
