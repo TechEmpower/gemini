a0f59452e5e8f293bc54b24c03e225c41a08a400
eluna-te <32777830+eluna-te@users.noreply.github.com>
2019-04-22 14:55:54 -0700

    Add a default servlet to the maven archetype (#50)
    * Add a default servlet to the maven archetype
    
    * Add webservlet annotation to server
    
    * Add missing import

--- gemini-resin-archetype/src/main/resources/archetype-resources/src/main/java/Server.java
+++ gemini-resin-archetype/src/main/resources/archetype-resources/src/main/java/Server.java
@@ -2,12 +2,14 @@ package ${package};
 
 import com.techempower.gemini.transport.*;
 import javax.servlet.annotation.MultipartConfig;
+import javax.servlet.annotation.WebServlet;
 
 /**
  * Interface to the application. 
  */
 @SuppressWarnings("serial")
 @MultipartConfig(fileSizeThreshold = 1024 * 1024 * 20, maxRequestSize = 1024 * 1024 * 50)
+@WebServlet(name="SRV", urlPatterns="*")
 public class Server
      extends InfrastructureServlet
 {
