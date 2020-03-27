f362a6c54fdf432f0560e557b0cf0716f1f06bd1
ajohnston <AlbertJohnstonCS@gmail.com>
2020-03-27 03:57:16 -0700

    Add log4j12 slf4j maven module; misc clean up

---
+++ gemini-log4j12/src/main/java/com/techempower/gemini/log/GeminiPatternLayout.java
@@ -0,0 +1,26 @@
+package com.techempower.gemini.log;
+
+import org.apache.log4j.EnhancedPatternLayout;
+import org.apache.log4j.spi.LoggingEvent;
+
+public class GeminiPatternLayout extends EnhancedPatternLayout
+{
+  @Override
+  public String format(LoggingEvent event)
+  {
+    Object message = event.getMessage();
+    if (message instanceof String)
+    {
+      String contextInfo = GeminiComponentLog.getContextInformation();
+      if (contextInfo != null && !contextInfo.isEmpty())
+      {
+        message = contextInfo + "- " + message;
+      }
+    }
+    return super.format(new LoggingEvent(event.getFQNOfLoggerClass(),
+        event.getLogger(), event.getTimeStamp(), event.getLevel(), message,
+        event.getThreadName(), event.getThrowableInformation(),
+        event.getNDC(), event.getLocationInformation(),
+        event.getProperties()));
+  }
+}
--- gemini-logback/src/main/java/com/techempower/gemini/log/RequestPatternConverter.java
+++ gemini-logback/src/main/java/com/techempower/gemini/log/RequestPatternConverter.java
@@ -7,7 +7,7 @@ import ch.qos.logback.classic.spi.ILoggingEvent;
  * Provides a logback pattern key for inserting request information into log
  * output like the old logging system would do behind the scenes. To use this,
  * include %req in the logback pattern. An example use of this can be found in
- * the Gemini-logback logback-sample.xml file.
+ * the Gemini-logback sample.logback.xml file.
  * The following must be included for the pattern to be recognized:
  * <p>
  * {@code <conversionRule conversionWord="req" converterClass="com.techempower.gemini.log.RequestPatternConverter" />}
